ab45bac2cba5bd40889317d63bb4ff9ef5c2408d
maxfil333_BT <maxfil333@gmail.com>
2024-06-28 08:59:49 +0300

    Exeptions traceback added

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ import time
 import shutil
 import msvcrt
 import argparse
+import traceback
 from glob import glob
 from itertools import count
 from natsort import os_sorted
@@ -29,63 +30,67 @@ def main(hide_logs=False, test_mode=True, use_existing=False, stop_when=0):
     grouped_files = group_files_by_name(files)
     c = count(1)
     for base, files in grouped_files.items():
-
-        # _____  CREATE JSON  _____
-        print('-'*20)
-        print('\nbase:', base, sep='\n')
-        print('files:', *files, sep='\n')
-        json_name = os.path.basename(base[0]) + '_' + '0' * 11 + '.json'
-        if base[-1] == 'pdf':
-            text_or_scanned_folder = config['NAME_text']
-            # __ RUN ASSISTANT __
-            if test_mode:
-                with open(config['TESTFILE'], 'r', encoding='utf-8') as file:
-                    result = file.read()
+        try:
+            # _____  CREATE JSON  _____
+            print('-'*20)
+            print('\nbase:', base, sep='\n')
+            print('files:', *files, sep='\n')
+            json_name = os.path.basename(base[0]) + '_' + '0' * 11 + '.json'
+            if base[-1] == 'pdf':
+                text_or_scanned_folder = config['NAME_text']
+                # __ RUN ASSISTANT __
+                if test_mode:
+                    with open(config['TESTFILE'], 'r', encoding='utf-8') as file:
+                        result = file.read()
+                else:
+                    result = run_assistant(files[0], hide_logs=hide_logs)
             else:
-                result = run_assistant(files[0], hide_logs=hide_logs)
-        else:
-            text_or_scanned_folder = config['NAME_scanned']
-            files.sort(reverse=True)
-            # __ RUN CHAT __
-            if test_mode:
-                with open(config['TESTFILE'], 'r', encoding='utf-8') as file:
-                    result = file.read()
-            else:
-                result = run_chat(*files, detail='high', hide_logs=hide_logs)
+                text_or_scanned_folder = config['NAME_scanned']
+                files.sort(reverse=True)
+                # __ RUN CHAT __
+                if test_mode:
+                    with open(config['TESTFILE'], 'r', encoding='utf-8') as file:
+                        result = file.read()
+                else:
+                    result = run_chat(*files, detail='high', hide_logs=hide_logs)
 
-        if result is None:
+            if result is None:
+                continue
+
+            json_path = os.path.join(date_folder, text_or_scanned_folder, json_name)
+            with open(json_path, 'w', encoding='utf-8') as file:
+                file.write(result)
+
+            # _____  COPY ORIGINAL FILE  _____
+            main_file = files[-1]
+            # изначальный тип файла (записанное в конце после _ )
+            original_file_type = os.path.basename(main_file).rsplit(".", 1)[0].rsplit("_", 1)[-1]
+            # изначальное имя файла без _pdf.pdf
+            original_file_name = os.path.basename(main_file).rsplit(".", 1)[0].rsplit("_", 1)[0]
+
+            original_file_full = original_file_name + '.' + original_file_type
+            original_file_path = os.path.join(config['IN_FOLDER'], original_file_full)
+            original_save_path = os.path.basename((os.path.splitext(original_file_path)[0]
+                                                   + f'_{original_file_type}'
+                                                   + os.path.splitext(original_file_path)[1]))
+            original_save_path = os.path.join(date_folder, text_or_scanned_folder, original_save_path)
+            shutil.copy(original_file_path, original_save_path)
+
+            # _____  CREATE HTML  _____
+            html_name = os.path.basename(base[0]) + '.html'
+            html_path = os.path.join(date_folder, text_or_scanned_folder, html_name)
+
+            create_html_form(json_path, html_path, original_save_path)
+
+            # _____  STOP ITERATION  _____
+            if stop_when > 0:
+                stop = next(c)
+                if stop == stop_when:
+                    break
+        except Exception as error:
+            print('ERROR!:', error)
+            traceback.print_exc()
             continue
-
-        json_path = os.path.join(date_folder, text_or_scanned_folder, json_name)
-        with open(json_path, 'w', encoding='utf-8') as file:
-            file.write(result)
-
-        # _____  COPY ORIGINAL FILE  _____
-        main_file = files[-1]
-        # изначальный тип файла (записанное в конце после _ )
-        original_file_type = os.path.basename(main_file).rsplit(".", 1)[0].rsplit("_", 1)[-1]
-        # изначальное имя файла без _pdf.pdf
-        original_file_name = os.path.basename(main_file).rsplit(".", 1)[0].rsplit("_", 1)[0]
-
-        original_file_full = original_file_name + '.' + original_file_type
-        original_file_path = os.path.join(config['IN_FOLDER'], original_file_full)
-        original_save_path = os.path.basename((os.path.splitext(original_file_path)[0]
-                                               + f'_{original_file_type}'
-                                               + os.path.splitext(original_file_path)[1]))
-        original_save_path = os.path.join(date_folder, text_or_scanned_folder, original_save_path)
-        shutil.copy(original_file_path, original_save_path)
-
-        # _____  CREATE HTML  _____
-        html_name = os.path.basename(base[0]) + '.html'
-        html_path = os.path.join(date_folder, text_or_scanned_folder, html_name)
-
-        create_html_form(json_path, html_path, original_save_path)
-
-        # _____  STOP ITERATION  _____
-        if stop_when > 0:
-            stop = next(c)
-            if stop == stop_when:
-                break
 
     # _____  RESULT MESSAGE  _____
     return (f'Сохранено {len(grouped_files.items())} x 3 = {len(grouped_files.items()) * 3} '
@@ -108,8 +113,9 @@ if __name__ == "__main__":
                               use_existing=args.use_existing,
                               stop_when=args.stop_when)
         print(f'\nresult_message:\n{result_message}\n')
-    except Exception as error:
-        print(error)
+    except Exception as global_error:
+        print('GLOBAL_ERROR!:', global_error)
+        traceback.print_exc()
 
     if getattr(sys, 'frozen', False):
         if args.no_exit:
