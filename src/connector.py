66e2c96cd118be6018d483d6a67a34c2a2ab9874
maxfil333_BT <maxfil333@gmail.com>
2024-10-29 15:30:27 +0300

    update http_connection function

--- src/connector.py
+++ src/connector.py
@@ -1,14 +1,15 @@
 import os
 import time
-from dotenv import load_dotenv
 import win32com.client
+from typing import Union
+from dotenv import load_dotenv
 
 import base64
 import requests
 from requests.auth import HTTPBasicAuth
 
-from config.config import config, NAMES
 from src.logger import logger
+from config.config import config, NAMES
 from src.utils import get_stream_dotenv
 
 load_dotenv(stream=get_stream_dotenv())
@@ -41,24 +42,53 @@ def response_to_deals(response: str) -> list[str] | None:
 
 # __________________ HTTP-REQUEST __________________
 
-def cup_http_request(function, *args, kappa=False):
-    user_1C = os.getenv('user_1C')
-    password_1C = os.getenv('password_1C')
+def cup_http_request(function, *args, kappa=True) -> Union[list, dict, None]:
+    user_1C = config['user_1C']
+    password_1C = config['password_1C']
 
+    # Определение серверов
     if kappa:
-        base = r'http://kappa5.group.ru:81/ca/hs/interaction/'
+        primary_base = r'http://kappa5.group.ru:81/ca/hs/interaction/'
+        secondary_base = r'http://10.10.0.10:81/ca/hs/interaction/'
     else:
-        base = r'http://10.10.0.10:81/ca/hs/interaction/'
+        primary_base = r'http://10.10.0.10:81/ca/hs/interaction/'
+        secondary_base = r'http://kappa5.group.ru:81/ca/hs/interaction/'
 
     function_args = r'/'.join(map(lambda x: base64.urlsafe_b64encode(x.encode()).decode(), args))
-    url = base + function + r'/' + function_args
-    logger.write(url)
 
-    response = requests.get(url, auth=HTTPBasicAuth(user_1C, password_1C))
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Ошибка: {response.status_code} - {response.reason}")
+    try:
+        # Формируем URL для первого сервера
+        primary_url = primary_base + function + r'/' + function_args
+        logger.print(f"Попытка запроса: {primary_url}")
+
+        # Попытка отправить запрос на первый сервер
+        response = requests.get(primary_url, auth=HTTPBasicAuth(user_1C, password_1C))
+
+        # Если первый запрос успешен, возвращаем результат
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logger.print(f"Ошибка при запросе к первому серверу: {response.status_code} - {response.reason}")
+    except Exception as error:
+        logger.print(error)
+
+    try:
+        # Формируем URL для второго сервера
+        secondary_url = secondary_base + function + r'/' + function_args
+        logger.print(f"Попытка запроса ко второму серверу: {secondary_url}")
+
+        # Попытка отправить запрос на второй сервер
+        response = requests.get(secondary_url, auth=HTTPBasicAuth(user_1C, password_1C))
+
+        # Возвращаем результат, если успешен
+        if response.status_code == 200:
+            return response.json()
+        else:
+            logger.print(f"Ошибка при запросе ко второму серверу: {response.status_code} - {response.reason}")
+            return None
+    except Exception as error:
+        logger.print(error)
+        return None
 
 
 if __name__ == '__main__':
