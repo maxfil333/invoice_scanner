44f352ca6a9503bc21fea27f264f65d8e1a3234d
maxfil333_BT <maxfil333@gmail.com>
2024-11-20 16:28:53 +0300

    reindex unique_comments.json

--- config/config.py
+++ config/config.py
@@ -64,6 +64,25 @@ except Exception as e:
     raise
 
 config['unique_comments_file'] = os.path.join(config['CONFIG'], 'unique_comments.json')
+
+
+def reindex_unique_comments(unique_comments_file_path: str) -> None:
+    """
+    Если вручную добавлялись/удалялись услуги, unique_comments нужно переиндексировать,
+    так как id словарика должен быть равен его порядковому номеру.
+    """
+    with open(unique_comments_file_path, 'r', encoding='utf-8') as f:
+        dct = json.load(f)
+        for i, d, in enumerate(dct):
+            d['id'] = i
+
+    with open(unique_comments_file_path, 'w', encoding='utf-8') as f:
+        json.dump(dct, f, ensure_ascii=False, indent=4)
+
+
+reindex_unique_comments(config['unique_comments_file'])
+
+
 config['unique_services'] = None  # to html <div id="services_dict..."
 config['not_found_service'] = 'Не найдено'
 try:
