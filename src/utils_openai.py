fd79c955f8aecd0f30f354058bccef8b381e46cc
maxfil333_BT <maxfil333@gmail.com>
2025-02-25 14:18:27 +0300

    feat(core): pdf_to_ai_details func added

--- src/utils_openai.py
+++ src/utils_openai.py
@@ -1,19 +1,26 @@
 import os
+import json
+
+from config.config import config, NAMES
 from src.logger import logger
-from src.utils import extract_text_with_fitz, extract_excel_text, count_extensions
-from src.main_openai import run_chat, run_assistant
+from src.utils import extract_text_with_fitz, extract_text_with_pdfplumber, extract_excel_text, count_extensions
+from src.main_openai import run_chat, run_assistant, run_chat_pydantic
+from src.models import ResponseDetails, processResponseDetails
 
 
-def pdf_to_ai(file: str, test_mode: bool, text_to_assistant: bool, config: dict, running_params: dict) -> str:
-    running_params['text_or_scanned_folder'] = config['NAME_text']
-    running_params['current_texts'] = extract_text_with_fitz(file)
-    running_params['doc_type'] = 'pdf'
+def pdf_to_ai(file: str, test_mode: bool, text_to_assistant: bool, config: dict, running_params: dict,
+              response_format=config['full_response_format']) -> str:
+
+    running_params.setdefault('text_or_scanned_folder', config['NAME_text'])
+    running_params.setdefault('current_texts', extract_text_with_fitz(file))
+    running_params.setdefault('doc_type', 'pdf')
+
     if test_mode:
         with open(config['TESTFILE'], 'r', encoding='utf-8') as f:
             return f.read()
     if not text_to_assistant:
         result = run_chat(file,
-                          response_format=config['response_format'], text_content=running_params['current_texts'])
+                          response_format=response_format, text_content=running_params['current_texts'])
         return result
     else:
         result = run_assistant(file)
@@ -28,7 +35,7 @@ def excel_to_ai(file: str, test_mode: bool, text_to_assistant: bool, config: dic
         with open(config['TESTFILE'], 'r', encoding='utf-8') as f:
             return f.read()
     result = run_chat('',
-                      response_format=config['response_format'], text_content=running_params['current_texts'])
+                      response_format=config['full_response_format'], text_content=running_params['current_texts'])
     return result
 
 
@@ -39,7 +46,7 @@ def images_to_ai(files: list, test_mode: bool, text_to_assistant: bool, config: 
         with open(config['TESTFILE'], 'r', encoding='utf-8') as f:
             return f.read()
     result = run_chat(*files,
-                      response_format=config['response_format'], text_content=None)
+                      response_format=config['full_response_format'], text_content=None)
     return result
 
 
@@ -63,7 +70,7 @@ def extra_excel_to_ai(extra_files: list,
                     with open(config['TESTFILE'], 'r', encoding='utf-8') as f:
                         return f.read()
                 result = run_chat('',
-                                  response_format=config['response_format'], text_content=running_params['current_texts'])
+                                  response_format=config['full_response_format'], text_content=running_params['current_texts'])
                 return result
 
 
@@ -75,7 +82,7 @@ def title_page_to_ai(title_path: str, test_mode: bool, text_to_assistant: bool, 
                 return f.read()
         if not text_to_assistant:
             result = run_chat(title_path,
-                              response_format=config['response_format'], text_content=running_params['title_page_texts'])
+                              response_format=config['full_response_format'], text_content=running_params['title_page_texts'])
             return result
         else:
             result = run_assistant(title_path)
@@ -85,5 +92,26 @@ def title_page_to_ai(title_path: str, test_mode: bool, text_to_assistant: bool, 
             with open(config['TESTFILE'], 'r', encoding='utf-8') as f:
                 return f.read()
         result = run_chat(title_path,
-                          response_format=config['response_format'], text_content=None)
+                          response_format=config['full_response_format'], text_content=None)
+        return result
+
+
+def pdf_to_ai_details(file: str, test_mode: bool, text_to_assistant: bool, config: dict, running_params: dict) -> str:
+    running_params.setdefault('text_or_scanned_folder', config['NAME_text'])
+    running_params.setdefault('current_texts', extract_text_with_fitz(file))
+    running_params.setdefault('current_texts_plumber', extract_text_with_pdfplumber(file))
+    running_params.setdefault('doc_type', 'pdf')
+    if test_mode:
+        with open(config['TESTFILE'], 'r', encoding='utf-8') as f:
+            result = f.read()
+            details_dct = {k: v for k, v in json.loads(result).items() if k in [NAMES.supplier, NAMES.customer]}
+            return json.dumps(details_dct)
+
+    if not text_to_assistant:
+        result = run_chat_pydantic(file,
+                                   response_format_pydantic=ResponseDetails,
+                                   text_content=running_params['current_texts_plumber'])
+        return processResponseDetails(result)
+    else:
+        result = run_assistant(file)
         return result
